f2a775fd792728ec22b82b306ebe7cb892b9a98b
Alejandro Ulloa <alejandroekas@gmail.com>
2023-02-23 15:18:49 -0500

    camera follow implemented

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position = player.position;
+        offset.y += 3;
+        offset.z -= 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = player.position + offset;
+        targetPosition.x = 0;
+        transform.position = targetPosition;
+    }
+}
